6075b5468f6f993f27589dabadfe2ed18ba3d2ec
M6a5x98 <130889212+M6a5x98@users.noreply.github.com>
2024-03-07 08:26:40 +0100

    Add control to check if the value provided is a number

--- casio-emulator/transpiler.js
+++ casio-emulator/transpiler.js
@@ -3,13 +3,17 @@ function transpile(code) {
     this.code = code.split("\n")
     for (let i = 0; i < this.code.length; i++) {
         const line = this.code[i];
-        if (!line.find("->") === -1) {
+        if (line.includes("->")) {
             const name = line.split("->")[0]
-            const value = line.split("->")[1]
+            let value;
+            if (parseFloat(line.split("->")[1]) === NaN) {
+              throw new TypeError("Value must be a number got a "+typeof line.split('->')[1])
+            }
+            vars.push({name:name, value:value})
         }
     }
+    console.log(vars)
 }
-
 /*
 a->12
 repeter 12 fois {
